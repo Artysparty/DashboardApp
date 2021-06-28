202210582f36f7791621ff9c08b45a598cc4d06f
Falkonny <62031372+Falkonny@users.noreply.github.com>
2021-06-28 12:49:56 +0300

    add auth service, add user auth model

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { UserAuthDTO } from 'src/app/models/user-auth.DTO';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly baseUrl = 'https://localhost:5000/api/login';
+
+  constructor(private http: HttpClient) {}
+
+  postUserAuth(userAuth: UserAuthDTO): Observable<UserAuthDTO> {
+    return this.http.post<UserAuthDTO>(this.baseUrl, userAuth);
+  }
+}
