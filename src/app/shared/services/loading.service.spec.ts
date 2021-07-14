dab9493af229fc1d8021d60ac4a4b391d88fade0
Falkonny <62031372+Falkonny@users.noreply.github.com>
2021-07-14 17:39:02 +0300

    small fix

--- src/app/shared/services/loading.service.spec.ts
+++ src/app/shared/services/loading.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { LoadingService } from './loading.service';
 
-describe('LoginService', () => {
+describe('LoadingService', () => {
   let service: LoadingService;
 
   beforeEach(() => {
